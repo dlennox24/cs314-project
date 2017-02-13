d5b7cfc910adb9b9faae4a51cee589c31d493bec
ap <ap@localhost.localdomain>
2017-02-13 14:37:24 -0700

    file reoginization. TripCo is now the main package with one class. Also contains Model Presenter and View packages

---
+++ src/Model/TestModel.java
@@ -0,0 +1,21 @@
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestModel {
+
+	@Before
+	public void initialize() {
+	}
+
+	@Test
+	public void testConditional() {
+		assertEquals(true, conditional));
+	}
+
+	@Test
+	public void testFunction() {
+		assertEquals(false, functionResult());
+	}
+}
