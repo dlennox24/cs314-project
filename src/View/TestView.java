58c0bf79b5348ed90f96c4e155f545d75ec1415b
Daniel Lennox <dlennox2424@gmail.com>
2017-02-13 20:16:39 -0700

    Adding tests for View.java

--- src/View/TestView.java
+++ src/View/TestView.java
@@ -1,21 +1,26 @@
-
+package View;
+import View.View;
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestView {
 
-	@Before
-	public void initialize() {
+	@Test
+	public void testPresenter() {
+		View view = new View();
+		assertEquals("OK",view.getStatus());
 	}
 
-	@Test
-	public void testConditional() {
-		assertEquals(true, conditional));
-	}
+//	@Test
+//	public void testConditional() {
+////		assertEquals(true, true);
+//	}
 
-	@Test
-	public void testFunction() {
-		assertEquals(false, functionResult());
-	}
+//	@Test
+//	public void testFunction() {
+//		
+//		assertEquals(false, display("test"));
+//	}
 }
--- src/View/View.java
+++ src/View/View.java
@@ -1,5 +1,20 @@
 package View;
 
-public interface View {
+import Presenter.Presenter;
 
+public class View {
+	private Presenter presenter;
+	private String status;
+	public View() {
+//		this.presenter = presenter;
+		this.status = "OK";
+	}
+	
+	public String getStatus(){
+		return this.status;
+	}
+	public String display(String text) {
+		System.out.println(text);
+		return text;
+	}
 }
