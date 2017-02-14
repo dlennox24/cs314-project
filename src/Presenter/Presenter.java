3edc6e523e1e8d57bd698322d8caf73b916fd468
Grant Johnson <gtjohnso@helena.cs.colostate.edu>
2017-02-13 20:21:12 -0700

    Adding Junit testing to Presenter

--- src/Presenter/Presenter.java
+++ src/Presenter/Presenter.java
@@ -1,5 +1,19 @@
 package Presenter;
 
-public class Presenter {
+import Model.Model;
+import View.View;
 
+public class Presenter {
+	private View view;
+	private Model model;
+	public Presenter(Model model) {
+	this.view
+	= view;
+	this.model
+	= model;
+	}
+	public void start() {
+	 model.getStatus();
+	}
+	
 }
--- src/Presenter/TestPresenter.java
+++ src/Presenter/TestPresenter.java
@@ -1,21 +1,19 @@
-
+package Presenter;
+import Model.Model;
+import View.View;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestPresenter {
 
-	@Before
-	public void initialize() {
-	}
-
 	@Test
-	public void testConditional() {
-		assertEquals(true, conditional));
+	public void notNull() {
+		assertNotNull(new Presenter(new Model("/s/bach/n/under/gtjohnso/cs314/DTR-27/src/Model/input.csv", "Decimal")));
 	}
-
 	@Test
-	public void testFunction() {
-		assertEquals(false, functionResult());
+	public void getStart() {
+		assertEquals("OK", new Model("/s/bach/n/under/gtjohnso/cs314/DTR-27/src/Model/input.csv", "Decimal").getStatus());
 	}
+	
 }
