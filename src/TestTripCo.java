da82c8639c5c1508921c4e8e5b285c07c871a403
Grant Johnson <gtjohnso@helena.cs.colostate.edu>
2017-02-13 20:25:21 -0700

    tripCo unit tests

--- src/TestTripCo.java
+++ src/TestTripCo.java
@@ -1,11 +1,19 @@
-
-import static org.junit.Assert.*;
-import org.junit.Test;
+import Presenter.Presenter;
 import Model.Model;
-
+import View.View;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TestTripCo {
+
+	@Test
+	public void notNull() {
+		assertNotNull(new Presenter(new Model("/s/bach/n/under/gtjohnso/cs314/DTR-27/src/Model/input.csv", "Decimal")));
+	}
+	@Test
+	public void getStart() {
+		assertEquals("OK", new Model("/s/bach/n/under/gtjohnso/cs314/DTR-27/src/Model/input.csv", "Decimal").getStatus());
+	}
 	
-
-
 }
