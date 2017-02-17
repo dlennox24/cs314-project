1305c6681fd88f378463d38f051d93db086cba3e
Daniel Lennox <dlennox2424@gmail.com>
2017-02-17 12:42:43 -0700

    Adding basic View object

--- src/View/View.java
+++ src/View/View.java
@@ -1,20 +1,45 @@
 package View;
 
-import Presenter.Presenter;
+import java.util.ArrayList;
 
 public class View {
-	private Presenter presenter;
 	private String status;
-	public View() {
-//		this.presenter = presenter;
+	private ArrayList<String> flags;
+	private String header;
+	private int totalDist;
+	
+	public View(String header, int totalDist, ArrayList<String> flags) {
 		this.status = "OK";
+		this.header = header;
+		this.totalDist = totalDist;
+		this.flags = flags;
+	}
+
+	public boolean addLeg(){
+		// add a leg to the SVG and XML file
+		// check for labels and distances
+		return false;
+	}
+
+	private boolean addLine(){
+		// add line to SVG and labels/distances if needed
+		return false;
+	}
+	
+	private boolean addDist(){
+		// Create the SVG distance label
+		return false;
+	}	
+
+	public boolean addLabel(){
+		// Add label to city locations in SVG
+		return false;
 	}
 	
 	public String getStatus(){
 		return this.status;
 	}
-	public String display(String text) {
-		System.out.println(text);
-		return text;
+	public void setStatus(String status){
+		this.status = status;
 	}
 }
