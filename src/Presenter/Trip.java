b89db2066139b9f13f80e22eb3d1025c7bede305
gtjohnso <gtjohnso@providence.cs.colostate.edu>
2017-02-20 19:44:19 -0700

    closes #32 closes #50 closes #49

--- src/Presenter/Trip.java
+++ src/Presenter/Trip.java
@@ -14,18 +14,26 @@ public class Trip{
 		
 	}
 	
-void createTrip(){
+void createTrip(String options){
 	
-	for(int i = 0; i < 4; i++){
-		TripList.add(this.Presenter.getModel().getLeg(i));
-		System.out.println(TripList.get(i));
-	}	
-	}
+		for(int i = 0; i < this.Presenter.getModel().getLegsLength(); i++){
+			
+			double lat1 = Presenter.getModel().getLeg(i).getLocations().get(0).getLat();
+			double lon1 = Presenter.getModel().getLeg(i).getLocations().get(0).getLong();
+			double lat2 = Presenter.getModel().getLeg(i).getLocations().get(1).getLat();
+			double lon2 = Presenter.getModel().getLeg(i).getLocations().get(1).getLong();
+			int distance = Presenter.getModel().getLeg(i).getDistance();
+			String name1 = Presenter.getModel().getLeg(i).getLocations().get(1).getName();
+			String name2 = Presenter.getModel().getLeg(i).getLocations().get(1).getName();
+			Presenter.getView().addLeg(lat1, lon1, lat2, lon2, distance, name1, name2);
+		
+		}	
+
+		
+	
+}
 public String toString(){
 	return this.TripList.toString();			
 }
-public ArrayList<Leg> getTripList(){
-		
-		return this.TripList;
-	}
 }
+
