086aed2ec00d090c0f3e9b5bff584b5e4a02fcb3
gtjohnso <gtjohnso@rams.colostate.edu>
2017-03-20 15:40:11 -0600

    Trying to get Travis to work
    closes #134

---
+++ src/OptionsGUI.java
@@ -0,0 +1,81 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import View.IteneraryDisplay;
+
+import javax.swing.JRadioButton;
+import javax.swing.JCheckBox;
+
+public class OptionsGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public OptionsGUI() {
+		
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		JTextField txtWelcomeToTripco = new JTextField();
+		txtWelcomeToTripco.setBounds(5, 5, 440, 29);
+		txtWelcomeToTripco.setFont(new Font("FreeMono", Font.BOLD, 25));
+		txtWelcomeToTripco.setEditable(false);
+		txtWelcomeToTripco.setText("Welcome To TripCo");
+		contentPane.add(txtWelcomeToTripco);
+		txtWelcomeToTripco.setColumns(10);
+		
+		JCheckBox chckbxMileage = new JCheckBox("Mileage");
+		chckbxMileage.setBounds(15, 36, 129, 23);
+		contentPane.add(chckbxMileage);
+		chckbxMileage.addActionListener(new ActionListener(){
+			
+			int count = 0;
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String cmd = e.getActionCommand();
+					
+					if(cmd.equals("Mileage")){
+						count++;
+						System.out.println(count);
+					
+					}
+					if(count % 2 == 1){
+						System.out.println("true");
+					}else{
+						System.out.println("false");
+					}
+				
+				}
+			
+			
+			});
+		
+	}
+	public void run() {
+		try {
+			OptionsGUI frame = new OptionsGUI();
+			frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/edu/csu2017sp314/DTR27/tripco/Model/Location.java
@@ -0,0 +1,58 @@
+package main.java.edu.csu2017sp314.DTR27.tripco.Model;
+
+
+
+public class Location {
+
+	private String id;
+	private String name;
+	private Double longitude;
+	private Double latitude;
+	
+	
+	public Location(String ID, String NAME, Double LONG, Double LAT){
+		
+		this.id = ID;
+		this.name = NAME;
+		this.longitude = LONG;
+		this.latitude = LAT;
+		
+	}
+	
+	public Location(String ID, String NAME, Double LONG, Double LAT, String EXTRA){
+		
+		this.id = ID;
+		this.name = NAME;
+		this.longitude = LONG;
+		this.latitude = LAT;
+		
+	}
+	
+	public String toString(){
+		
+		return this.id + " , " + this.name + " , " + this.longitude.toString() + " , "+ this.latitude.toString();
+		
+	}
+	
+	public String getId(){return this.id;}
+	public void setId(String ID){this.id = ID;}
+	
+	public String getName(){return this.name;}
+	public void setName(String NAME){this.name = NAME;}
+	
+	public Double getLong(){return this.longitude;}
+	public void setLong(Double LONG){this.longitude = LONG;}
+
+	public Double getLat(){return this.latitude;}
+	public void setLat(Double LAT){this.latitude = LAT;}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		String s = "ABCDMSFSDFasdasdasd122344567890!@#!$@$#%^$&&&*".replaceAll("[A-Za-z]", " ");
+		System.out.println(s);
+	}
+
+}
