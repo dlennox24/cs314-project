5ebf9aed09322569e91d690c094d83bb9e5bf893
Daniel Lennox <dlennox2424@gmail.com>
2017-04-02 19:25:19 -0600

    Fixing CC complaining about single letter variables

--- src/main/java/edu/csu2017sp314/DTR27/tripco/Model/Optimizer.java
+++ src/main/java/edu/csu2017sp314/DTR27/tripco/Model/Optimizer.java
@@ -33,8 +33,8 @@ public class Optimizer {
 		return newRoute;
 	}
 	
-	double calcDist(Location a, Location b){
-		return new Leg().greatCircleDistanceDouble(a, b);
+	double calcDist(Location start, Location end){
+		return new Leg().greatCircleDistanceDouble(start, end);
 	}
 	
 	ArrayList<Leg> twoOpt(ArrayList<ArrayList<Leg>> allTours) {
