6c6d363cd26b272b0d0bf612a5b553f043787475
Daniel Lennox <dlennox2424@gmail.com>
2017-04-19 11:26:20 -0600

    (#190, #191) Adding basic structure for itinerary and settings

--- src/ui/src/components/App.js
+++ src/ui/src/components/App.js
@@ -9,25 +9,59 @@ class Map extends Component {
   }
 }
 
+class Itinerary extends Component{
+  render() {
+    return (
+      <div>
+        <h3>Itinerary
+        <a type="button" className="pull-right" data-toggle="modal" data-target="#myModal">
+          <i className="fa fa-sliders"></i>
+          </a>
+        </h3>
+      </div>
+    );
+  }
+}
+
+class Settings extends Component{
+  render() {
+    return (
+      <div className="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div className="modal-dialog modal-lg" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 className="modal-title" id="myModalLabel">Settings</h4>
+            </div>
+            <div className="modal-body">
+              ...
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 export default class App extends Component{
   render() {
     return (
       <Layout>
         <div className='row'>
-          <div className='white-grey-bg-color col-md-2 sidebar'>
+          <div className='white-grey-bg-color col-md-3'>
             <Sidebar>
-              <h3>Itinerary</h3>
+              <Itinerary />
             </Sidebar>
           </div>
-          <div className='white-grey-bg-color col-md-8'>
+          <div className='col-md-9'>
             <Map />
           </div>
-          <div className='white-grey-bg-color col-md-2 sidebar'>
-            <Sidebar>
-              <h3>Settings</h3>
-            </Sidebar>
-          </div>
         </div>
+        <Settings />
       </Layout>
     );
   }
